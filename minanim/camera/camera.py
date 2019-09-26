4dcb1470702bca9473df4fcc1cc360759c1f0903
BrandonRawlings <brandonrawlings@gmail.com>
2019-09-26 06:13:24 -0700

    continued setting up virtual environments

--- minanim/camera/camera.py
+++ minanim/camera/camera.py
@@ -10,11 +10,17 @@ import cairo
 class Camera(object):
 
     def __init__(self, frame_rate=30):
-        self.frame_rate = 30
+        self.daframe_rate = 30
+        self.ctx = None
+        self.pixel_height = None
+        self.pixel_width = None
+        self.aperture_height = None
+        self.aperture_width = None
 
     @property
+
     def frame_rate(self):
-        return self.frame_rate
+        return self.daframe_rate
 
     @frame_rate.setter
     def frame_rate(self, rate):
@@ -24,10 +30,13 @@ class Camera(object):
 
 
     def display_frame_from_file(self, file):
+        ''' Opens a file from the
+        '''
         pass
 
     def capture_frame_to_file(self):
 
         pass
+
 class MovieCamera(Camera):
     pass
--- minanim/camera/test_camera.py
+++ minanim/camera/test_camera.py
@@ -2,30 +2,6 @@ import unittest
 import camera
 import hashlib
 
-class TestMovieCamera(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        pass
-
-    def setUp(self):
-        '''set up some instances to test'''
-        self.cam1 = camera.MovieCamera(frame_rate=30)
-
-    def tearDown(self):
-        pass
-
-    def test_capture_frame(self):
-
-        pass
-
-    def test_frame_rate(self):
-        self.assertGreater(self.cam1.frame_rate,0)
-
-        with self.assertRaises(ValueError):
-            self.cam1.frame_rate = 0
-            self.cam1.frame_rate = -3
-
 class TestCamera(unittest.TestCase):
 
     @classmethod
@@ -34,23 +10,27 @@ class TestCamera(unittest.TestCase):
 
     def setUp(self):
         '''set up some instances to test'''
-        self.cam1 = camera.MovieCamera(frame_rate=30)
+        self.cam1 = camera.Camera(frame_rate=30)
 
     def tearDown(self):
         pass
 
+
+    def test_frame_rate(self):
+        self.assertGreater(self.cam1.frame_rate,0)
+
+        with self.assertRaises(ValueError):
+            self.cam1.frame_rate = 0
+            self.cam1.frame_rate = -3
+
     def test_capture_frame(self):
-        with open('test_image.png','r') as f:
+        with open('media/test/tulips.png','r') as f:
             image_at_aperture = self.cam1.display_frame_from_file(f)
-            captured_frame = self.cam1.capture_frame(image_at_aperture)
-            re_image = self.cam1.display_frame_from_file(captured_frame)
-
+            capt_frame = self.cam1.capture_frame_to_file()
+            re_image = self.cam1.display_frame_from_file(capt_frame)
+            def gethash(x): hashlib.md5(x).hexdigest()
             self.assertEqual(gethash(image_at_aperture),
-                             gethash(re_image)
-                             )
-    def
-
-
+                             gethash(re_image))
 
 #redirect to unittest to run its main script
 if __name__ == "__main__":
